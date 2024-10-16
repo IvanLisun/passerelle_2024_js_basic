8610350ddb10f656be9b62697e0796d82ce2bd36
IvanLisun <114064849+IvanLisun@users.noreply.github.com>
2024-10-16 09:31:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function howILikeToEat (fruits) {
+    if (fruits.includes("dorado")) {
+        fruits[fruits.indexOf("dorado")] = "fish";
+    }
+
+    let sentence1 = fruits.join(", ") + " I only eat that in summer.";
+    let sentence2 = fruits.join(", ") + " I only eat that once a week.";
+    let sentence3 = fruits.join(", ") + " I do not eat";
+    
+    if (fruits.includes("apples") || fruits.includes("mango")) {
+        return sentence1;
+    }
+    
+    else if (fruits.includes("dorado") || fruits.includes("beaf") || fruits.includes("tofu")) {
+        return sentence2;
+    }
+    
+    else {
+        return sentence3;
+    }
+}
+
+document.write(howILikeToEat(['apples','mango']) + '<br>');
+document.write(howILikeToEat(['dorado','beaf','tofu']) + '<br>');
+document.write(howILikeToEat(['computer']) + '<br>');
